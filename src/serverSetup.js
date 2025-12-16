1475f49e0375d99c1d523d4e3668cad635b4d63c
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-12-16 12:51:24 -0500

    feat: add routes for react client to use

--- src/serverSetup.js
+++ src/serverSetup.js
@@ -1,5 +1,6 @@
 import { env } from "node:process";
 import { default as express } from "express";
+import crypto from "crypto";
 import { setTimeout } from "node:timers/promises";
 import { exit } from "process";
 import cors from "cors";
@@ -18,7 +19,7 @@ expressApp.use(express.json());
 
 expressApp.use(
   cors({
-    origin: env.CLIENT_ORIGIN,
+    origin: process.env.CLIENT_ORIGIN,
     credentials: true, // allow cookies,
     allowedHeaders: ["Content-Type", "Authorization"], // may not need to specify Authorization
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // may not need this
@@ -46,8 +47,8 @@ function parseResponse(response) {
 async function exchangeCode(code) {
   console.log("in exchangeCode");
   const params = new URLSearchParams({
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
     code: code,
   });
 
@@ -69,10 +70,22 @@ expressApp.get("/", (req, res) => {
   });
 });
 
+expressApp.get("/signin", async (req, res) => {
+  const githubAPI = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+  const redirectUri = `${process.env.CLIENT_ORIGIN}/github/client`; // your backend callback route
+  const scope = "read:user user:avatar user:handle";
+  const state = crypto.randomBytes(16).toString("hex");
+  // TODO store the state in the user's session or profile in the db for comparison on callback (defend against CSRF attack)
+
+  res.redirect(`${githubAPI}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}&state=${state}`);
+})
+
 expressApp.get("/github", async (req, res) => {
   console.log("in the github callback route");
   const code = req.query.code;
 
+  // TODO compare the state with the one we saved in the session? (or in the user's account somehow to confirm it is the same)
+  const state = req.query.state;
   //res.render("login", { message: `Successfully authorized! Got code ${code}.` });
 
   try {
@@ -104,6 +117,39 @@ expressApp.get("/github", async (req, res) => {
   }
 });
 
+expressApp.get("/github/client", async (req, res) => {
+  console.log("in the github/client callback route");
+  const code = req.query.code;
+
+  // TODO compare the state with the one we saved in the session? (or in the user's account somehow to confirm it is the same)
+  const state = req.query.state;
+
+  try {
+    const result = await exchangeCode(code);
+    console.log("Access token response:", result);
+    if (result["access_token"]) {
+      // store the access_token AND the refresh_token!
+      const token = result["access_token"];
+      const userInfo = await getUserInfo(token);
+
+      console.log(userInfo);
+      const handle = userInfo.login; //hbar1st for myself for eg
+      const avatar_url = userInfo.avatar_url;
+
+      res.status(200).json({
+        message: `Successfully authorized! Welcome, (${handle}).`,
+        handle: handle,
+        avatar: avatar_url,
+      });
+    } else {
+      res.status(400).json({
+        message: `Authorized, but unable to exchange code ${code} for token. It may have expired?`,
+      });
+    }
+  } catch (error) {
+    console.error("Error exchanging code:", error);
+  }
+});
 async function getUserInfo(token) {
   console.log("in getUserInfo");
   const response = await fetch("https://api.github.com/user", {
